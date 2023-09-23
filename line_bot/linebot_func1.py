791ba6f7d705fe59e452650183d728bae6829b33
wty81213 <wty81213@hotmail.com>
2023-09-23 20:56:16 +0800

    fixed linebot_func1

--- line_bot/linebot_func1.py
+++ line_bot/linebot_func1.py
@@ -86,7 +86,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, message)
         
         except:
-            line_bot_api.reply_message(reply_token, TextSendMessage(text = "發生錯誤!!"))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "發生錯誤!!"))
     
     elif mtext == "@傳送位置":
         try:
